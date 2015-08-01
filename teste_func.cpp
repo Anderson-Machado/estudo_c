6ed2b03b921c5510f76a49eec95840990ceef794
Anderson Machado <machado.analista@gmail.com>
2015-08-01 00:16:14 -0300

    Envio

---
+++ teste_func.cpp
@@ -0,0 +1,32 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int par(int n)
+{   
+	if(n % 2==0)
+	{
+	return 1; 	
+	}
+	else
+	{
+	return 0;
+	}
+		
+}
+
+main()
+{
+	int i;
+	printf("informe um numero");
+	 scanf("%d",&i);
+	  if(par(i)==1)
+	  {
+	  	printf("Par");
+	  }
+	  else
+	  {
+	  	printf("Impar");
+	  }
+	  
+	system("pause");
+}
